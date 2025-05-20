13c019fa0502d2f46efc77fb166b8f2b7ff688c8
ai-ngabo <a.ngabo@alustudent.com>
2025-05-20 22:31:07 +0200

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -33,6 +33,7 @@ class BaseModel:
     def save(self):
         """ Updates the public instance attributes """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """ returns a dictionary containing all keys/values of the instance
