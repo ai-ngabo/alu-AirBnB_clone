6f0a8a2e34bdc2a4870606a76f8021addab5a3b0
ai-ngabo <a.ngabo@alustudent.com>
2025-05-20 22:37:08 +0200

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if kwargs:
+            kwargs.pop('__class__', None)
             for key, value in kwargs.items():
                 if key in ('created_at', 'updated_at'):
                     setattr(self, key, datetime.fromisoformat(value))
