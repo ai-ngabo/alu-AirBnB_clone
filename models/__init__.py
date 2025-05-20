dab353d3be7b40e5433895ac09239e29083c2961
ai-ngabo <a.ngabo@alustudent.com>
2025-05-20 22:51:59 +0200

    Created BaseModel in CLI

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,25 @@
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
-storage = FileStorage()  # Create an instance of FileStorage
-storage.reload()  # Load existing data from file.json
+storage = FileStorage()
+storage.reload()
+
+# Define `classes` dictionary for use in console.py
+classes = {
+    "BaseModel": BaseModel,
+    "User": User,
+    "State": State,
+    "City": City,
+    "Amenity": Amenity,
+    "Place": Place,
+    "Review": Review
+}
+
+# Store loaded objects from FileStorage
+loaded_objects = storage.all()
