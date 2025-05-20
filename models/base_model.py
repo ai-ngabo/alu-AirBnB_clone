c870cb7c46ac7cd7988597b22a72bed803267229
ai-ngabo <a.ngabo@alustudent.com>
2025-05-20 12:38:52 +0200

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,18 @@ class BaseModel:
     A base class that defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+        else:
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ returns a human readable str form of the class instance & attr"""
