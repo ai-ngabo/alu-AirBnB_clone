6ea920470eb3d3f9065eb5f2af03108b69f1307f
ai-ngabo <a.ngabo@alustudent.com>
2025-06-06 22:53:42 +0200

    debugging

--- models/base_model.py
+++ models/base_model.py
@@ -1,51 +1,40 @@
-#!/usr/bin/python3
-"""
-This module holds a model that defines the all common attr/metthods
-for other class
-"""
 import uuid
 from datetime import datetime
 import models
-
 class BaseModel:
-    """
-    A base class that defines all common attributes/methods for other classes
-    """
-
     def __init__(self, *args, **kwargs):
         if kwargs:
-            kwargs.pop('__class__', None)
-            for key, value in kwargs.items():
-                if key in ('created_at', 'updated_at'):
-                    setattr(self, key, datetime.fromisoformat(value))
-                else:
-                    setattr(self, key, value)
-
+            for key, val in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'uodated_at']:
+                        setattr(self, key, datetime.fromisoformat(val))
+                    else:
+                        setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """ returns a human readable str form of the class instance & attr"""
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """ Updates the public instance attributes """
         self.updated_at = datetime.now()
         models.storage.save()
-
+        
+ 
     def to_dict(self):
-        """ returns a dictionary containing all keys/values of the instance
-        used for data serialization and deserialization
-        """
-        the_dict = {}
-        the_dict['__class__'] = self.__class__.__name__
+        
+        instance_dict = self.__dict__.copy()
+        instance_dict["__class__"] = self.__class__.__name__
+        instance_dict["created_at"] = self.created_at.isoformat()
+        if isinstance(self.updated_at, str):
+            instance_dict["updated_at"] = self.updated_at
+        else:
+            instance_dict["updated_at"] = self.updated_at.isoformat()
 
-        for key, val in self.__dict__.items():
-            if key in ('created_at', 'updated_at'):
-                the_dict[key] = val.isoformat()  # convert to str obj
-            else:
-                the_dict[key] = val
-        return the_dict
+        for key, value in self.__dict__.items():
+            if key not in ['created_at', 'updated_at']:
+                instance_dict[key] = value
+        return instance_dict
